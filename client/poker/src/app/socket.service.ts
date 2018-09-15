bbc0103c77ae36f9437ffe094e88baed71da6104
Brad <bradwilder31415@gmail.com>
2018-09-15 13:09:13 -0600

    Remove some test code

--- client/poker/src/app/socket.service.ts
+++ client/poker/src/app/socket.service.ts
@@ -11,12 +11,6 @@ export class SocketService
 		let socketIOPort = 3000;
 		let socketIOUrl = window.location.protocol + '//' + window.location.hostname + ':' + socketIOPort;
 		this.socket = io(socketIOUrl);
-		
-		// TODO: this is just for testing
-		this.socket.on('event', (data) =>
-		{
-			console.log('on event', data);
-		});
 	}
 	
 	getSocket()
